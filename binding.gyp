3983eb05fdb5451d95bc2351bfb3d41675ec5921
Ross Leitch <professional.balbatross@gmail.com>
2021-09-30 22:08:09 +1300

    Round 2

---
+++ binding.gyp
@@ -0,0 +1,22 @@
+"binding.gyp" 
+{
+	"targets": [
+		{
+			"target_name": "native",
+			"sources": [
+				"src/binding/binding.cpp",
+				"src/piTest/piControlIf.c"
+			],
+			"include_dirs": [
+				"../",
+				"<!@(node -p \"require('node-addon-api').include\")"
+			],
+			"dependencies": [
+				"<!(node -p \"require('node-addon-api').gyp\")"
+			],
+			"cflags!": ["-I.", "-I..", "-fno-exceptions"],
+			"cflags_cc!": ["-fno-exceptions"],
+			"defines": ["NAPI_CPP_EXCEPTIONS"]
+		}
+	]
+}
---
+++ src/binding/binding.cpp
@@ -0,0 +1,30 @@
+#define __cplusplus 201112L
+#include <napi.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <termios.h>
+#include <getopt.h>
+#include <time.h>
+#include <sys/types.h>
+#include <stdbool.h>
+
+#include "piControlIf.h"
+#include "piControl.h"
+using namespace Napi;
+
+Napi::String Method(const Napi::CallbackInfo& info) {
+          Napi::Env env = info.Env();
+          showDeviceList();
+            return Napi::String::New(env, "world");
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) {
+          exports.Set(Napi::String::New(env, "HelloWorld"),
+                                        Napi::Function::New(env, Method));
+            return exports;
+}
+
+NODE_API_MODULE(addon, Init)
