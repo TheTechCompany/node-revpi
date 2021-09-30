6caba31762c2ad63d7352d84c4c720c4e57d4af6
Ross Leitch <professional.balbatross@gmail.com>
2021-09-30 22:37:10 +1300

    Round 2

--- src/binding/binding.cpp
+++ src/binding/binding.cpp
@@ -32,11 +32,12 @@ Napi::Object ShowDeviceList(const Napi::CallbackInfo& info){
 
 	int devCount = piControlGetDeviceInfoList(devices);
 	for(int dev = 0; dev < devices.length; dev++){
-		char *name = getModuleName(devices[dev].i16uModuleType & PICONTROL_NOT_CONNECTED_MASK)
+		char *name = getModuleName(devices[dev].i16uModuleType & PICONTROL_NOT_CONNECTED_MASK);
+		
 		Napi::Object obj = Napi::Object::New(env);
 		obj.Set(Napi::String::New(env, "name"), name);
 
-		device_list.Set(Napi::Number::new(env, dev), obj);
+		device_list.Set(Napi::Number::New(env, dev), obj);
 	} 
 
 }
