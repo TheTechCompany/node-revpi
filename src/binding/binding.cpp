12d089d243e1985da4f865cc1926a34cb93e444c
Ross Leitch <professional.balbatross@gmail.com>
2021-10-14 10:42:17 +1300

    trying ts bindings

--- src/binding/binding.cpp
+++ src/binding/binding.cpp
@@ -60,8 +60,11 @@ Napi::Value readValue(const Napi::CallbackInfo& info){
 	char* var = const_cast<char*>(variableName.c_str());
 
 	uint8_t* val = readVariableValue(var, false, 'd', false);
+	double double_val;
 
-	Napi::Value value = Napi::Number::New(env, (double)val[0]);
+	memcpy(&double_val, val, sizeof(val));
+
+	Napi::Value value = Napi::Number::New(env, double_val);
 	return value;
 }
 
